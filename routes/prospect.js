d4ee3565452e77efbfa7ba63359d6090d08e23b5
SohaibArbiBakcha <bakchalona@gmail.com>
2021-09-03 12:40:36 +0100

    fix func transform in prospect

--- routes/prospect.js
+++ routes/prospect.js
@@ -4,6 +4,7 @@ const {
   createStudents,
   getProspectById,
   updateProspect,
+  deleteProspect,
 } = require("../controllers/prospect");
 
 const router = express.Router();
@@ -11,6 +12,7 @@ router.post("/inscription", createStudents);
 router.get("/prospect", getStudents);
 router.put("/prospect/:prosId", updateProspect);
 router.get("/prospect/:prosId", getProspectById);
+router.delete("/prospect/:prosId", deleteProspect);
 
 router.param("prosId", getProspectById);
 module.exports = router;
