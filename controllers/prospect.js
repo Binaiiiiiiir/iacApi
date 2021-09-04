e175391d56c2a64df015aed1103f4a8da65f1ca4
SohaibArbiBakcha <bakchalona@gmail.com>
2021-09-04 11:12:57 +0100

    change courses display

--- controllers/prospect.js
+++ controllers/prospect.js
@@ -43,16 +43,18 @@ exports.createStudents = async (req, res) => {
 exports.getStudents = (req, res) => {
   Prospect.find()
     .populate("city", "name")
-    .populate("cours", "  name")
+    // .populate("cours", "  name")
 
     .then((data) => {
       let formatData = [];
       res.set("Content-Range", `0-10/${data.length}`);
       for (let i = 0; i < data.length; i++) {
         // let city = data[i].city.transform();
-        for (let j = 0; j < data[i].cours.length; j++) {
-          data[i].cours[j] = data[i].cours[j].transform();
-        }
+
+        // for (let j = 0; j < data[i].cours.length; j++) {
+        //   data[i].cours[j] = data[i].cours[j].transform();
+        // }
+
         // data[i].city = city;
         // console.log(city, data[i].city);
 
