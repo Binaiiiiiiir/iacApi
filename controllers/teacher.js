65abd2bb4c86d32e1c75f810f93e6af28066553d
SohaibArbiBakcha <bakchalona@gmail.com>
2021-09-25 18:19:29 +0100

    fix a bug

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -52,7 +52,7 @@ exports.getTeacher = (req, res) => {
   }
   if (filter.course) {
     filter.course = {
-      $in: mongoose.Types.ObjectId(filter.cours),
+      $in: mongoose.Types.ObjectId(filter.course),
     };
   }
   if (filter.id) {
