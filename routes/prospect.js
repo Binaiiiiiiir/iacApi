4817804bdde4fafcb15d271c9f1c97395025bcaf
SohaibArbiBakcha <bakchalona@gmail.com>
2021-09-26 12:38:41 +0100

    add 204  to getProspectOne

--- routes/prospect.js
+++ routes/prospect.js
@@ -11,8 +11,8 @@ const {
 const router = express.Router();
 router.post("/inscription", createProspect);
 router.get("/prospect", getProspects);
-router.put("/prospect/:prosId", updateProspect);
 router.get("/prospect/:prosId", getProspectById);
+router.put("/prospect/:prosId", updateProspect);
 router.delete("/prospect/:prosId", deleteProspect);
 
 router.param("prosId", getProspectOne);
